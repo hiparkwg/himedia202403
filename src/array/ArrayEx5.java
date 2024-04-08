341a622bfbe679e55292d369ac5cde5378d03144
jobtc <hiparkwg@gmail.com>
2024-04-08 15:20:23 +0900

    응용5

---
+++ src/array/ArrayEx5.java
@@ -0,0 +1,29 @@
+package array;
+
+import java.util.Scanner;
+
+public class ArrayEx5 {
+    public ArrayEx5(){
+        String[] names=  {"김","이","박","최","홍", "남"};
+        String[] address={"서울", "부산", "대구","서울", 
+                          "서울", "성남"};
+        Scanner s = new Scanner(System.in);
+        while(true){
+            System.out.print("검색할 주소는 ? ");
+            String addr = s.nextLine();
+            if(addr.equals("exit")) break;
+            for(int i=0; i<address.length; i++){
+                if(addr.equals(address[i])){ // addr==address[i]
+                    System.out.println(names[i]);
+                }
+            }
+            System.out.println("-".repeat(40));
+        }                          
+                          
+
+
+    }
+    public static void main(String[] args) {
+        ArrayEx5 ex = new ArrayEx5();
+    }
+}
