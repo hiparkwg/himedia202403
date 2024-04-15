c622fc3e20c268d2089e42b2f5206796daf6bdb2
jobtc <hiparkwg@gmail.com>
2024-04-15 11:27:19 +0900

    제품정보 조회및 제품코드로 제품명 조회

--- src/product/ProductSearch.java
+++ src/product/ProductSearch.java
@@ -1,6 +1,36 @@
 package product;
-
+import java.util.*;
 public class ProductSearch {
+    // 제품코드 또는 제품명의 일부분으로 검색
+    public void search(){
+        Scanner s = new Scanner(System.in);
+        boolean flag=true;
+        String findStr="";
+        while(flag){ //검색에 대한 반복
+            System.out.print("검색어(exit=종료) >>> ");
+            findStr = s.nextLine();
+            if(findStr.equals("exit")){
+                flag=false;
+                continue;
+            }
+
+            System.out.println("-".repeat(40));
+            System.out.println("No   code   codeName    ea  price   amount");
+            System.out.println("-".repeat(40));
+
+            for(int i=0 ; i<ProductMain.cnt ; i++){
+                ProductVo vo = ProductMain.products[i];
+                String codeName = findCodeName(vo.getCode());
+                if(vo.getCode().contains(findStr) ||
+                   codeName.contains(findStr) ){
+                        System.out.println(vo.print());
+                   }
+            }
+        }
+    }
+
+
+    //제품코드를 사용하여 제품명 검색
     public static String findCodeName(String code){
         String codeName="";
 
