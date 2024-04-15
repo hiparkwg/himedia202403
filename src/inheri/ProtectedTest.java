a3a48ec2a50e29ed4d49b33f1aa6324b612dd4fd
jobtc <hiparkwg@gmail.com>
2024-04-15 17:58:49 +0900

    상속관계가 있거나 같은 패키지내에서만 접근 가능한 protected 타입 선언

---
+++ src/inheri/ProtectedTest.java
@@ -0,0 +1,11 @@
+/* 
+ * 상속관계가 있거나 같은 패키지내에서만 접근 가능한
+ * protected 타입 선언
+ */
+package inheri;
+
+public class ProtectedTest {
+    protected String pwd="1234";
+    String id="hong";//같은 패키지내에서만 가능
+
+}
