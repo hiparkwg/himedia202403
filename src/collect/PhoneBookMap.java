a2851114a7e5db2e3cbfe560f98fd12cc49de3d5
jobtc <hiparkwg@gmail.com>
2024-04-23 14:04:41 +0900

    Map 을 사용한 PhoneBook CRUD

---
+++ src/collect/PhoneBookMap.java
@@ -0,0 +1,18 @@
+package collect;
+
+public class PhoneBookMap {
+    public void insert(){}
+    public void search(){}
+    public void update(){}
+    public void delete(){}
+
+    public void addPhoneNo(String groupName, String name, String tel){}
+    public void addPhoneNo(String name, String tel){}
+    public void addGroup(String groupName){}
+    public void printList(){}
+
+    public static void main(String[] args) {
+        PhoneBookMap p = new PhoneBookMap();
+
+    }
+}
