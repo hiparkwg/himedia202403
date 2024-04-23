e197f0f3c9df9c62075514f6eda1db86bee93769
jobtc <hiparkwg@gmail.com>
2024-04-23 12:31:23 +0900

    HashSet에서 CRUD

--- src/collect/HashSetEx2.java
+++ src/collect/HashSetEx2.java
@@ -3,11 +3,55 @@ import java.util.*;
 public class HashSetEx2 {
     public void test(){
         Set set = new HashSet();
+        // C(Create, insert)
         set.add("dog");
         set.add("cat");
         set.add("dog");
         set.add(new Person("홍길동", 20));
         set.add(new Person("홍길동", 20));
+        set.add(new Person("일지매", 20));
+        set.add(new Person("홍길동", 30));
+        System.out.println(set);
+
+        //R(Read, select, search)
+        Iterator it = set.iterator();
+        while(it.hasNext()){
+            Object o = it.next();
+            System.out.println(o);
+        }
+
+        //U(Update)
+        Person p = new Person("일지매", 20);
+        Iterator it2 = set.iterator();
+        while(it2.hasNext()){
+            Object o2 = it2.next();
+            if(o2 instanceof Person){
+                Person p2 = (Person)o2;
+                if(p2.equals(p)){
+                    p2.phone = "010-1111-1111";
+                    p2.score =100;
+                }
+            }
+        }
+        System.out.println("수정후.....");
+        System.out.println(set);
+        
+        //Update 두번째 방법
+        p=new Person("일지매", 20);
+        p.phone = "010-4444-4444";
+        if(set.contains(p)){
+            set.remove(p);
+        }
+        set.add(p);
+        System.out.println("두번째 방법으로 수정 후.");
+        System.out.println(set);
+
+        
+        //D(delete, remove)
+        if(set.contains(p)){
+            set.remove(p);
+        }
+        System.out.println("삭제후....");
         System.out.println(set);
 
     }
@@ -19,12 +63,15 @@ public class HashSetEx2 {
 class Person{
     String mName;
     int    age;
+    String phone;
+    int score;
     Person(String name, int age){
         this.mName = name;
         this.age = age;
     }
     public String toString(){
-        return mName + ":" + age;
+        return mName + ":" + age + ",phone=" + phone 
+               + ", score=" + score ;
     }
 
     @Override
